99c42ddf241e63f45406d1db99266ff2d5f2a1b4
Thomas Vandercam <thomasvandercam@Thomass-MacBook-Pro.local>
2020-08-27 09:40:49 +0200

    test(): food controller base

--- src/food/food.controller.spec.ts
+++ src/food/food.controller.spec.ts
@@ -1,18 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FoodController } from './food.controller';
+import { FoodService } from './food.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Food, FoodSchema } from './schemas/food.schema';
+import { CreateFoodDto } from './dto/create-food.dto';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { FoodType } from './models/food-type.enum';
+
 
 describe('FoodController', () => {
-  let controller: FoodController;
+  const mongod = new MongoMemoryServer();
+  let app: TestingModule;
+  let foodController: FoodController;
+  const mockFoodModel: CreateFoodDto = {
+    title: 'Belgian fries',
+    foodType: FoodType.Vegetables
+  };
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const uri = await mongod.getUri();
+
+    app = await Test.createTestingModule({
+      imports: [
+      MongooseModule.forRoot(`${uri}`), MongooseModule.forFeature([{ name: Food.name, schema: FoodSchema }])],
       controllers: [FoodController],
+      providers: [FoodService],
     }).compile();
 
-    controller = module.get<FoodController>(FoodController);
+    foodController = app.get<FoodController>(FoodController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('root', () => {
+    it('should create a new Food', async () => {
+      const res: any = await foodController.createFoodDto(mockFoodModel);
+
+      expect(res._id).toBeDefined();
+      expect(res.title).toEqual(mockFoodModel.title);
+    });
   });
+
+  afterAll(async (done) => {
+    await mongod.stop();
+    done();
+  })
 });
